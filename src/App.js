a44d7b2adc0850b0d231562f25bd10d8aabc24b0
rizgoing <rizgoing@gmail.com>
2020-06-05 14:02:46 +0300

    Create HomePage and add a Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
+import Home from "./components/Home";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route path="/" component={Home} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>There is epty page. Please, go to Users to view some pretty page :)</p>
+    </div>
+  );
+};
+
+export default Home;
